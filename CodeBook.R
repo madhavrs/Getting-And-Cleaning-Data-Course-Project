686fb5d2a0b8b2323702f47a2c9f7e368ca81804
unknown <ctsuser1@CTSTX33191.cts.com>
2015-05-24 18:26:56 -0400

    committing

---
+++ CodeBook.R
@@ -0,0 +1,21 @@
+#Code Book for "Getting and Cleaning Data" Course Project
+
+#Description
+
+This code book describes the variables, the data, and any transformations done to clean up the data 
+
+#Source of Data
+
+Human Activity Recognition Using Smartphones Data Set that is available at UCI Machine Learning Repository.  The data was extracted from the Human Activity Recognition database available for public use at UCI, built from the recordings of 30 subjects performing activities of daily living (ADL) while carrying a waist-mounted smartphone with embedded inertial sensors
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones#
+
+#Data Format 
+
+The key applicable variables in of this dataset are: 
+
+SUBJECT - To identify which of the 30 subjects carried out the activity 
+ACTIVITY - A factor/label describing the activity performed (LAYING / SITTING / WALKING / STANDING / WALKING DOWNSTAIRS / WALKING UPSTAIRS)
+MEASUREMENT VARIABLE - Describes the type of measurement taken from the accelerometer and gyroscope.  79 FACTOR LEVELS are used to describe the same.  
+AVERAGE - Describes the mean calculated from the measurements 
+STD DEV - Describes the standard deviation calculated from the measurements 
